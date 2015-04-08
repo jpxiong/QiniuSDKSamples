d12e6536d453cde756370f47babba6369627c0ae
jpxiong <xiongjipu@gmail.com>
2015-04-08 23:47:17 +0800

    rename some var names

--- QiniuJavaSDKSampleV6.1.7.1/src/com/jerikc/qiniudemo/javasdk/uploadfile/UploadFile.java
+++ QiniuJavaSDKSampleV6.1.7.1/src/com/jerikc/qiniudemo/javasdk/uploadfile/UploadFile.java
@@ -6,15 +6,15 @@ import com.qiniu.api.io.PutRet;
 
 public class UploadFile {
 
-        private final static String LOCAL_FILE_PATH = "/Users/jerikc/test.txt";
-        private final static String FILE_NAME_ON_QINIU = "test.txt";
+        private final static String LOCAL_FILE_FULL_PATH = "/Users/jerikc/test.txt";
+        private final static String FILE_NAME_ON_CLOUD = "test.txt";
 
-        public void uploadFileToQiniu(String filePath, String fileName) {
+        public void uploadFileToQiniu(String fullPath, String fileNameOnCloud) {
                 String upToken = Authorization.getAuthToken();
                 PutExtra putExtra = new PutExtra();
                 
                 // push the file into the Qiniu cloud storage
-                PutRet putRet = IoApi.putFile(upToken, fileName, filePath, putExtra);
+                PutRet putRet = IoApi.putFile(upToken, fileNameOnCloud, fullPath, putExtra);
                 
                 System.out.println(putRet.getKey()); //the file name on Qiniu cloud storage
         }
@@ -22,7 +22,7 @@ public class UploadFile {
         public static void main(String args[]) {
                 UploadFile uploadFile = new UploadFile();
                 try {
-                        uploadFile.uploadFileToQiniu(LOCAL_FILE_PATH, FILE_NAME_ON_QINIU);
+                        uploadFile.uploadFileToQiniu(LOCAL_FILE_FULL_PATH, FILE_NAME_ON_CLOUD);
                 } catch(Exception e) {
                         e.printStackTrace();
                 }
