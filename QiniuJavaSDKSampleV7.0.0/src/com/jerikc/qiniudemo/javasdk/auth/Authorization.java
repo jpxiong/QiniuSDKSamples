bfb6806df4264049aac8dbccb1257e5f4ad5f720
jpxiong <xiongjipu@gmail.com>
2015-04-09 11:02:37 +0800

    update the ak/sk

--- QiniuJavaSDKSampleV7.0.0/src/com/jerikc/qiniudemo/javasdk/auth/Authorization.java
+++ QiniuJavaSDKSampleV7.0.0/src/com/jerikc/qiniudemo/javasdk/auth/Authorization.java
@@ -3,13 +3,9 @@ package com.jerikc.qiniudemo.javasdk.auth;
 import com.qiniu.util.Auth;
 
 public class Authorization {
-//        public static final String ACCESS_KEY = "your ak";
-//        public static final String SECRET_KEY = "your sk";
-//        public static final String BUCKET_NAME = "your bucket";
-        
-		public static final String ACCESS_KEY = "-DyVIaGWHtncdap_3MSNbCJ_aVY7LMWD4Dgq6Z1I";
-	    public static final String SECRET_KEY = "feZC1S5L6Lg-qkpR1l0DUR-9kLSmI1K-5liKkHqd";
-	    public static final String BUCKET_NAME = "testspace";
+        public static final String ACCESS_KEY = "your ak";
+        public static final String SECRET_KEY = "your sk";
+        public static final String BUCKET_NAME = "your bucket";
 	    
 	    private Authorization() {
 	    	
